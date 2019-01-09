a83df74bf3669c95c3b8bc5d2efea179548df002
evgen.ru79 <evgen.ru79@gmail.com>
2019-01-09 06:50:41 +0500

    Android tests is OK

--- app/src/androidTest/kotlin/net/pot8os/kotlintestsample/ApplicationTest.kt
+++ app/src/androidTest/kotlin/net/pot8os/kotlintestsample/ApplicationTest.kt
@@ -1,5 +1,6 @@
 package net.pot8os.kotlintestsample
 
+import android.util.Log.e
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
@@ -7,10 +8,12 @@ import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.rule.ActivityTestRule
 import androidx.test.runner.AndroidJUnit4
+import io.reactivex.Completable
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import java.text.DecimalFormat
+import java.util.concurrent.TimeUnit
 
 /**
  * @author So Nakamura, 2015/12/19
@@ -18,64 +21,72 @@ import java.text.DecimalFormat
 @RunWith(AndroidJUnit4::class)
 class ApplicationTest {
 
-  @Rule
-  @JvmField
-  val activity = ActivityTestRule<MainActivity>(MainActivity::class.java)
+    @Rule
+    @JvmField
+    val activity = ActivityTestRule<MainActivity>(MainActivity::class.java)
 
-  @Test
-  fun testAdd() {
-    onView(withId(R.id.button_1)).perform(click())
-    onView(withId(R.id.button_2)).perform(click())
-    onView(withId(R.id.button_3)).perform(click())
-    onView(withId(R.id.button_add)).perform(click())
-    onView(withId(R.id.button_3)).perform(click())
-    onView(withId(R.id.button_2)).perform(click())
-    onView(withId(R.id.button_1)).perform(click())
-    onView(withId(R.id.button_calc)).perform(click())
-    onView(withId(R.id.field))
-      .check(matches(withText("${123 + 321}")))
-  }
+    @Test
+    fun testAdd() {
 
-  @Test
-  fun testSub() {
-    onView(withId(R.id.button_9)).perform(click())
-    onView(withId(R.id.button_9)).perform(click())
-    onView(withId(R.id.button_9)).perform(click())
-    onView(withId(R.id.button_sub)).perform(click())
-    onView(withId(R.id.button_3)).perform(click())
-    onView(withId(R.id.button_3)).perform(click())
-    onView(withId(R.id.button_3)).perform(click())
-    onView(withId(R.id.button_calc)).perform(click())
-    onView(withId(R.id.field))
-      .check(matches(withText("${999 - 333}")))
-  }
+        // TODO Issue Android Test
+        e("-----TEST------", "asdasd\n222".split("\n").toString())
 
-  @Test
-  fun testMulti() {
-    onView(withId(R.id.button_1)).perform(click())
-    onView(withId(R.id.button_0)).perform(click())
-    onView(withId(R.id.button_0)).perform(click())
-    onView(withId(R.id.button_multi)).perform(click())
-    onView(withId(R.id.button_2)).perform(click())
-    onView(withId(R.id.button_0)).perform(click())
-    onView(withId(R.id.button_0)).perform(click())
-    onView(withId(R.id.button_calc)).perform(click())
-    val formatter = DecimalFormat("#,###")
-    onView(withId(R.id.field))
-      .check(matches(withText(formatter.format(100 * 200))))
-  }
+        onView(withId(R.id.button_1)).perform(click())
+        onView(withId(R.id.button_2)).perform(click())
+        onView(withId(R.id.button_3)).perform(click())
+        onView(withId(R.id.button_add)).perform(click())
+        onView(withId(R.id.button_3)).perform(click())
+        onView(withId(R.id.button_2)).perform(click())
+        onView(withId(R.id.button_1)).perform(click())
+        onView(withId(R.id.button_calc)).perform(click())
+        onView(withId(R.id.field))
+            .check(matches(withText("${123 + 321}")))
+    }
 
-  @Test
-  fun testDiv() {
-    onView(withId(R.id.button_3)).perform(click())
-    onView(withId(R.id.button_3)).perform(click())
-    onView(withId(R.id.button_3)).perform(click())
-    onView(withId(R.id.button_divide)).perform(click())
-    onView(withId(R.id.button_1)).perform(click())
-    onView(withId(R.id.button_0)).perform(click())
-    onView(withId(R.id.button_0)).perform(click())
-    onView(withId(R.id.button_calc)).perform(click())
-    onView(withId(R.id.field))
-      .check(matches(withText("${333 / 100.0}")))
-  }
+    @Test
+    fun testSub() {
+
+        // TODO Issue Android Test
+        val issue2 = Completable.complete().delay(200, TimeUnit.MILLISECONDS)
+
+        onView(withId(R.id.button_9)).perform(click())
+        onView(withId(R.id.button_9)).perform(click())
+        onView(withId(R.id.button_9)).perform(click())
+        onView(withId(R.id.button_sub)).perform(click())
+        onView(withId(R.id.button_3)).perform(click())
+        onView(withId(R.id.button_3)).perform(click())
+        onView(withId(R.id.button_3)).perform(click())
+        onView(withId(R.id.button_calc)).perform(click())
+        onView(withId(R.id.field))
+            .check(matches(withText("${999 - 333}")))
+    }
+
+    @Test
+    fun testMulti() {
+        onView(withId(R.id.button_1)).perform(click())
+        onView(withId(R.id.button_0)).perform(click())
+        onView(withId(R.id.button_0)).perform(click())
+        onView(withId(R.id.button_multi)).perform(click())
+        onView(withId(R.id.button_2)).perform(click())
+        onView(withId(R.id.button_0)).perform(click())
+        onView(withId(R.id.button_0)).perform(click())
+        onView(withId(R.id.button_calc)).perform(click())
+        val formatter = DecimalFormat("#,###")
+        onView(withId(R.id.field))
+            .check(matches(withText(formatter.format(100 * 200))))
+    }
+
+    @Test
+    fun testDiv() {
+        onView(withId(R.id.button_3)).perform(click())
+        onView(withId(R.id.button_3)).perform(click())
+        onView(withId(R.id.button_3)).perform(click())
+        onView(withId(R.id.button_divide)).perform(click())
+        onView(withId(R.id.button_1)).perform(click())
+        onView(withId(R.id.button_0)).perform(click())
+        onView(withId(R.id.button_0)).perform(click())
+        onView(withId(R.id.button_calc)).perform(click())
+        onView(withId(R.id.field))
+            .check(matches(withText("${333 / 100.0}")))
+    }
 }
